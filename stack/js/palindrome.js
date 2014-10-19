144d2491a43c2e8b73c5914c3339019312b14920
tidycode <doomscity@qq.com>
2014-10-19 23:12:35 +0800

    add palindrome demo
    添加palindrome 的示例

--- stack/js/palindrome.js
+++ stack/js/palindrome.js
@@ -1,13 +1,13 @@
 /**
  * 判断字符串是否是回文
- * @param  {[type]}  str [description]
- * @return {Boolean}     [description]
+ * @param  {string}  str 判断的字符串
+ * @return {Boolean}     true or false
  */
 function isPalinedrome(str) {
 	var oldStr = str.split("");
 	var len = oldStr.length;
 	var newStr = "";
-	var s = new stack();
+	var s = new Stack();
 	var i = 0;
 	for (; i < len; ++i) {
 		s.push(oldStr[i]);
