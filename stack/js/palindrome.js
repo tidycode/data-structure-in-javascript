6df9f5e320930e998fc1a2600a3e615fb6247ecc
tidycode <doomscity@qq.com>
2014-10-19 23:26:40 +0800

    using single var
    遵循单一VAR法则

--- stack/js/palindrome.js
+++ stack/js/palindrome.js
@@ -4,16 +4,16 @@
  * @return {Boolean}     true or false
  */
 function isPalinedrome(str) {
-	var oldStr = str.split("");
-	var len = oldStr.length;
-	var newStr = "";
-	var s = new Stack();
-	var i = 0;
+	var oldStr = str.split(""),
+		len = oldStr.length,
+		newStr = "",
+		s = new Stack(),
+		i = 0;
 	for (; i < len; ++i) {
 		s.push(oldStr[i]);
 	};
 	while (s.length() > 0) {
 		newStr += s.pop();
 	}
-	return newStr === str?true:false;
+	return newStr === str ? true : false;
 }
